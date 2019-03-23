7bc4c8b1a3a09109c024ea0d336f8da57eecf53c
Ayush Choudhury <choudhuryaayush@gmail.com>
2019-03-23 13:33:40 +0530

    Bird Controls added

--- Flappy Bird/Assets/playerController.cs
+++ Flappy Bird/Assets/playerController.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public  float speed;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-         if(Input.GetButtonDown("Fire1"))
+          rb.velocity = new Vector2(2f, 0f);
+         if(Input.GetMouseButtonDown(0))
         {
-            rb.AddForce(new Vector2(0f, 2f));
+            rb.velocity = Vector2.zero;
+            rb.AddForce(new Vector2(0f, speed), ForceMode2D.Impulse);
+
         }
         else{
             rb.AddForce(new Vector2(0f, 0f));
         }
-        
+
     }
 }
